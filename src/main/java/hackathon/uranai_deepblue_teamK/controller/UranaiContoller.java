3edb1e1878013a073c3837fa8d441abde7a97129
gomibuchimaika <maika.gomibuchi@rakus.co.jp>
2016-07-23 16:18:57 +0900

    serviceクラス作成

--- src/main/java/hackathon/uranai_deepblue_teamK/controller/UranaiContoller.java
+++ src/main/java/hackathon/uranai_deepblue_teamK/controller/UranaiContoller.java
@@ -1,17 +1,24 @@
 package hackathon.uranai_deepblue_teamK.controller;
 
 import hackathon.uranai_deepblue_teamK.form.InputForm;
+import hackathon.uranai_deepblue_teamK.service.UranaiService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 /**
  * Created by rkuser on 2016/07/23.
  */
 @Controller
 public class UranaiContoller {
+	
+	@Autowired
+	private UranaiService uranaiService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String get(){
@@ -20,6 +27,7 @@ public class UranaiContoller {
 
     @RequestMapping(value = "/uranau", method = RequestMethod.POST)
     public String uranau(@ModelAttribute("Uranai") InputForm inputForm, BindingResult bindingResult){
+    	String imgPath = uranaiService.uranai(inputForm.getName(), inputForm.getBirth().toString());
         return "output.jsp";
     }
 
---
+++ src/main/java/hackathon/uranai_deepblue_teamK/service/UranaiService.java
@@ -0,0 +1,48 @@
+package hackathon.uranai_deepblue_teamK.service;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UranaiService {
+ 
+	/**
+	 * Unicode文字列に変換する("あ" -> "\u3042")
+	 * @param original
+	 * @return
+	 */
+	private static int convertToUnicode(String original)
+	{
+		int total = 0;
+	    StringBuilder sb = new StringBuilder();
+	    for (int i = 0; i < original.length(); i++) {
+	    	String str = String.format("\\u%04X", Character.codePointAt(original, i));
+	        total += changeNum(str);
+	    }
+	    return total;
+	}
+	
+	private static int changeNum(String num){
+		
+		int i = 0;
+		try{
+			i = Integer.parseInt(num.replace("\\u", ""), 16);
+		}catch(Exception ex){
+			System.err.println("error");
+		}
+		return i;
+	}
+	
+	public String uranai(String name, String birth){
+		int num = convertToUnicode(name);
+		int num2 = Integer.parseInt(birth);
+		int answer = (num + num2)%27;
+		String img = null;
+		if(String.valueOf(answer).length() > 1){
+			img = "/img/0" + answer + ".gif";
+		}else{
+			img = "/img/00" + answer + ".gif";
+		}
+		return img;
+	}
+}
