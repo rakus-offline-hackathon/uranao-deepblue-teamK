280365c2b483cd5b8eacf35ab7b8b519a0480c88
t.sato <sato_tadaaki@yahoo.co.jp>
2016-07-23 13:22:02 +0900

    new

---
+++ src/main/java/hackathon/uranai_deepblue_teamK/App.java
@@ -0,0 +1,13 @@
+package hackathon.uranai_deepblue_teamK;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
